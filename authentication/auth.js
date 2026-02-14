7d43d9f06ac00d1967c5ea554aca1ace361c714a
Hamza Malik <hamzamalik123450@gmail.com>
2026-02-14 20:26:50 +0500

    update commit

--- authentication/auth.js
+++ authentication/auth.js
@@ -1,26 +1,30 @@
-// Regex for validation 
-import {emailValidation, passwordvalidate } from './validation.js'
+const validateEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+const passwordValid =  password.length >= 8
+
 
 const emailAlerts = document.getElementById('emailalert');
 const passwordAlerts = document.getElementById('passwordalert');
 const confirmAlerts = document.getElementById('confirmalert');
-const password = document.getElementById('password');
+// const password = document.getElementById('password');
 
 const form = document.getElementById('signupform');
-const submitBtn = document.getElementById('submitbtn');
+// const submitBtn = document.getElementById('submitbtn');
 
-submitBtn.addEventListener('submit', async (e) => {
+form.addEventListener('submit', async (e) => {
+    console.log(form);
+    
     e.preventDefault(); 
 
-    const email = form.email.value;
-    const password = form.password;
-    const confirmPassword = form.confirmPassword.value; 
 
-    if(!emailValidation(email)){
+    const email = form.email;
+    const password = form.password;
+    const confirmPassword = form.confirmPassword; 
+
+    if(!validateEmail.test(email)){
         emailAlerts.innerText = 'Please enter a valid email address.';
         return;
     }
-    if(!passwordvalidate(password)){
+    if(!passwordValid.test(password)){
         passwordAlerts.innerText = 'Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, and one number.';
         return;
     }
--- authentication/validation.js
+++
@@ -1,9 +0,0 @@
-export const emailValidation = (email) =>{
-    const validateEmail = '/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/'
-    return validateEmail.test(email)   
-}
-
-export const passwordvalidate = (password) =>{
-    return password.length >= 8
-}
-
